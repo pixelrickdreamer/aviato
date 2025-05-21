7420a7129a1b87661daa5a5d87f5768e5fd4eb43
pixelrickdreamer <jakob.sanne-grubbstrom@migrationsverket.se>
2025-05-21 12:34:34 +0200

    initial vite app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aviato
+# Aviato
+
+![img](https://www.filmgarb.com/uploads/tj_miller-tv-silicon_valley-erlich_bachmann-0101-t_shirts-erlichs_aviato_shirt.jpg)
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import {Tag, TagGroup, Label} from '@midas-ds/components'
+
+import './App.css'
+import '@midas-ds/components/global.css'
+
+function App() {
+
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      <div className={'group'}>
+        <Label>Border</Label>
+      <TagGroup className={'standardTags'}>
+        <Tag className={'tag green'}>Standard tag</Tag>
+        <Tag className={'tag blue'}>Standard tag</Tag>
+        <Tag className={'tag'}>Standard tag</Tag>
+        <Tag className={'tag red'}>Standard tag</Tag>
+      </TagGroup>
+      </div>
+      <div className={'group'}>
+        <Label>BoxShadow</Label>
+        <TagGroup className={'boxShadow'}>
+          <Tag className={'green'}>Standard tag</Tag>
+          <Tag className={'tag box-shadow green'}>Standard tag</Tag>
+          <Tag className={'tag box-shadow'}>Standard tag</Tag>
+          <Tag className={'tag red box-shadow'}>Standard tag</Tag>
+        </TagGroup>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
